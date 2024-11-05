dfb04fbd966c2dc8838e75a7b916cbed9d85e0fa
GwenDumoulin <gwen.dumoulin@gmail.com>
2024-11-05 15:47:08 +0100

    feat: add TimelineSlider component for audio timeline control

---
+++ src/components/TimelineSlider.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="timeline-slider">
+    <input
+      type="range"
+      min="0"
+      :max="duration"
+      v-model="currentTime"
+      @input="onSeek"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  props: {
+    currentTime: {
+      type: Number,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+  },
+  emits: ['seek'],
+  setup(props, { emit }) {
+    const onSeek = () => {
+      emit('seek', props.currentTime);
+    };
+
+    return { onSeek };
+  },
+});
+</script>
+
+<style scoped>
+/* Style pour le slider */
+</style>
