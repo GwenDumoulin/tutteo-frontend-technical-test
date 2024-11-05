edd7e6a96682f18996457322ac66b07b987a874e
GwenDumoulin <gwen.dumoulin@gmail.com>
2024-11-05 15:30:42 +0100

    feat: add PlayerControls component with play, pause, stop buttons

---
+++ src/components/PlayerControls.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="player-controls">
+    <button @click="play" v-if="!isPlaying">Play</button>
+    <button @click="pause" v-if="isPlaying">Pause</button>
+    <button @click="stop">Stop</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  isPlaying: Boolean,
+});
+
+const emit = defineEmits(['play', 'pause', 'stop']);
+
+const play = () => emit('play');
+const pause = () => emit('pause');
+const stop = () => emit('stop');
+</script>
+
+<style scoped>
+/* 
+  .player-controls {
+
+  }
+  */
+</style>
